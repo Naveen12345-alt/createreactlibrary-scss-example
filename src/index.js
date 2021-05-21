3b5244b7afad1f7086eeef52d18173c7fed1790f
Naveen12345-alt <naveenchauhan1234@protonmail.com>
2021-05-21 22:57:26 +0530

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import styles from './styles.module.css'
+import './styles.scss'
+export * from './Button'
+export * from './SignUpBox'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <div className='test'>Example Component: {text}</div>
 }
